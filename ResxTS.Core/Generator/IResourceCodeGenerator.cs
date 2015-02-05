368c572d876c53a6a8fd3fc77870300c4cea0ed7
Torsten Holtse <torsten.holtse@live.dk>
2015-02-05 22:02:28 +0100

    Initial checkin

---
+++ ResxTS.Core/Generator/IResourceCodeGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxTS.Core.Generator
+{
+    public interface IResourceCodeGenerator
+    {
+        public string Generate(string @namespace, ResxResult mainResx, ResxResult localizedResx = null);
+
+        public void Generate(Stream stream, string @namespace, ResxResult mainResx, ResxResult localizedResx = null);
+    }
+}
---
+++ ResxTS.Core/ResxResult.cs
@@ -0,0 +1,44 @@
+﻿using ResxTS.Core.Reader;
+using ResxTS.Core.Scanner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxTS.Core
+{
+    public class ResxResult
+    {
+        private Dictionary<string, ResxValue> entries = new Dictionary<string, ResxValue>();
+
+        public string Name { get; private set; }
+        public string Culture { get; private set; }
+        public bool IsDefault { get; private set; }
+
+        public IReadOnlyDictionary<string, ResxValue> Entries
+        {
+            get { return entries; }
+        }
+
+        public ResxResult(string name, string culture, bool isDefault = false)
+        {
+            Name = name;
+            Culture = culture;
+            IsDefault = isDefault;
+        }
+
+        public static ResxResult Create<T>(IResxReader<T> reader) where T : ResxEntry
+        {
+            var scanner = new StringValueScanner();
+            var result = new ResxResult(reader.Name, reader.Culture, isDefault: string.IsNullOrWhiteSpace(reader.Culture));
+
+            foreach (var item in reader.GetEntries())
+            {
+                result.entries.Add(item.Key, scanner.Scan(item.Value));
+            }
+
+            return result;
+        }
+    }
+}
